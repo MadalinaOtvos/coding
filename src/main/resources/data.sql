590a54a4609edb7e18af4793a67e34968b269e8c
MadalinaOtvos <otvos_madalina@yahoo.com>
2018-12-06 21:16:55 +0200

    In memory database population at startup.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into bikes (id,name,latitude,longitude,rented) values (1,'',50.119504,8.638137, false);
+insert into bikes (id,name,latitude,longitude,rented) values (2,'',50.119229,8.640020, false);
+insert into bikes (id,name,latitude,longitude,rented) values (3,'',50.120452,8.650507, false);
+insert into users (id,name,email,password) values (1,'Mike','mike@yahoo.com', 'mike');
+insert into users (id,name,email,password) values (2,'Tom','tom@yahoo.com', 'tom');
+insert into users (id,name,email,password) values (3,'Ane','ane@yahoo.com', 'ane');
