5880f41df1558873a651858200e01b2b34325e85
MadalinaOtvos <otvos_madalina@yahoo.com>
2018-12-09 22:59:09 +0200

    Constants for header settings and token properties.

---
+++ src/main/java/com/bike/rental/model/Constants.java
@@ -0,0 +1,9 @@
+package com.bike.rental.model;
+
+public class Constants {
+
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final String SIGNING_KEY = "ojid46FKJf55JFPw556wWkoeijhedTjid7D555D";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
