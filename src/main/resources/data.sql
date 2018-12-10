cec3af943e867b23a88d524a021af0f0cb707392
MadalinaOtvos <otvos_madalina@yahoo.com>
2018-12-10 03:38:59 +0200

    Bike entries updated, extra column email added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into bikes (id,name,latitude,longitude,rented) values (1,'',50.119504,8.638137, false);
-insert into bikes (id,name,latitude,longitude,rented) values (2,'',50.119229,8.640020, false);
-insert into bikes (id,name,latitude,longitude,rented) values (3,'',50.120452,8.650507, false);
+insert into bikes (id,name,latitude,longitude,rented,email) values (1,'test',50.119504,8.638137, true,'test@yahoo.com');
+insert into bikes (id,name,latitude,longitude,rented,email) values (2,'',50.119229,8.640020, false,'');
+insert into bikes (id,name,latitude,longitude,rented,email) values (3,'',50.120452,8.650507, false,'');
