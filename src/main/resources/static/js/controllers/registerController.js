66ff5b30e36851ebef820cafe2602ebe62e733f5
MadalinaOtvos <otvos_madalina@yahoo.com>
2018-12-12 13:24:31 +0200

    Registration handling already registered user, redirecting it to log in page.

--- src/main/resources/static/js/controllers/registerController.js
+++ src/main/resources/static/js/controllers/registerController.js
@@ -1,30 +1,37 @@
-    'use strict';
+'use strict';
 
-    angular
-        .module('bikeRental')
-        .controller('RegisterController', RegisterController);
+angular
+    .module('bikeRental')
+    .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = ['$rootScope','$location','RegistrationService','NotificationService'];
-    function RegisterController($rootScope,$location,RegistrationService,NotificationService) {
+RegisterController.$inject = ['$rootScope', '$location', 'RegistrationService', 'NotificationService'];
 
-        var self = this;
+function RegisterController($rootScope, $location, RegistrationService, NotificationService) {
+    var vm = {
+        submit: submit
+    };
 
-        self.submit = submit;
-        self.user = {};
-        self.user.email = "";
-        self.user.username = "";
+    return vm;
 
-        function submit() {
-            console.log('Submitting registration...');
-            RegistrationService.register(self.user)
-                .then(function(response) {
+    vm.user = {};
+    vm.user.email = "";
+    vm.user.username = "";
+
+    function submit() {
+        console.log('Submitting registration...');
+        RegistrationService.register(vm.user)
+            .then(function (response) {
+                console.log(response.data);
                 $location.url("/");
-                NotificationService.Success("Registration successful! Please log in!",true)
-            }).catch(function(error){
-               console.log("Error while registering user: " + error);
-               NotificationService.Error("Error occurred while registering the user! Please try again!",false);
-            });
-
-
-        }
+                NotificationService.Success("Registration successful! Please log in!", true);
+            }).catch(function (error) {
+            if (error.status === 409) {
+                $location.url("/");
+                NotificationService.Error("You are already registered! Please try to log in!", true);
+            } else {
+                console.log("Error while registering user: " + error);
+                NotificationService.Error("Error occurred while registering the user! Please try again!", false);
+            }
+        });
     }
+}
--- src/main/resources/static/js/services/registrationService.js
+++ src/main/resources/static/js/services/registrationService.js
@@ -2,32 +2,50 @@
     'use strict';
     angular
         .module('bikeRental')
-        .factory('RegistrationService', RegistrationService);
-        RegistrationService.$inject = ['$http','$q','API_URL'];
+        .service('RegistrationService', RegistrationService);
+    RegistrationService.$inject = ['$http', '$q', 'API_URL'];
 
-         function RegistrationService($http,$q, API_URL) {
+    function RegistrationService($http, $q, API_URL) {
 
-            var service = {
-                register: register
-            };
+        var service = {
+           register: register,
+           unregister: unregister
+        };
 
-            return service;
+        return service;
 
-            function register(user) {
-                console.log('Trying to register user ' + user);
-                var deferred = $q.defer();
-                $http.post(API_URL + "/users/register", user)
-                    .then(
-                        function (response) {
-                            console.log("User successfully created!");
-                            deferred.resolve(response);
-                        },
-                        function (errResponse) {
-                            console.error('Error while creating entry : '+ errResponse.data);
-                            deferred.reject(errResponse);
-                        }
-                    );
-                return deferred.promise;
-            }
+        function register(user) {
+            console.log('Trying to register user ' + user);
+            var deferred = $q.defer();
+            $http.post(API_URL + "/users/register", user)
+                .then(
+                    function (response) {
+                        console.log("User successfully created!");
+                        deferred.resolve(response);
+                    },
+                    function (errResponse) {
+                        console.error('Error while creating entry : ' + user.email + "\n Details: " + errResponse.data);
+                        deferred.reject(errResponse);
+                    }
+                );
+            return deferred.promise;
         }
+
+        function unregister(user) {
+            console.log('Trying to deregister user ' + user);
+            var deferred = $q.defer();
+            $http.delete(API_URL + "/users/" + user.id)
+                .then(
+                    function (response) {
+                        console.log("User unregistered successfully!");
+                        deferred.resolve(response);
+                    },
+                    function (errResponse) {
+                        console.error('Error while creating entry : ' + user.email + "\n Details: " + errResponse.data);
+                        deferred.reject(errResponse);
+                    }
+                );
+            return deferred.promise;
+        }
+    }
 }());
